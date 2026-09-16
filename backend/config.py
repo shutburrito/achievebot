f892ebf83cf7cbf5357cfff35ce4fe3290f022fb
Kristian Mills <millskristian50@gmail.com>
2026-09-16 20:28:25 +0000

    heroku deployment

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv #allows the program to access the .env folder for variables
-load_dotenv() #loads .env 
+
+if os.getenv("DYNO") is None:
+    load_dotenv() #loads .env 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN") #these get the variables from the .env, but gives the defaults if it cant find it
 BACKEND_URL = os.getenv("BACKEND_URL", "http://localhost:8000")
